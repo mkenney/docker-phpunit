792b83d7d8c4e3d42bdeb9b2e33d43162329dc50
Michael Kenney <michael.kenney@returnpath.net>
2016-07-08 15:52:01 -0600

    The build didn't trigger, adjusting curl calls

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env sh
 
 if [ "master" = "$TRAVIS_BRANCH" ]; then
-    DOCKER_TAG=latest
-else
-    DOCKER_TAG=$TRAVIS_BRANCH
+    echo "Triggering builds for tags 'latest' and 'php7'"
+    curl -H "Content-Type: application/json" --data '{"docker_tag": "latest"}' -X POST https://registry.hub.docker.com/u/mkenney/phpunit/trigger/6cfc768e-a07b-48c1-890c-24bbd071261c/
+    curl -H "Content-Type: application/json" --data '{"docker_tag": "php7"}' -X POST https://registry.hub.docker.com/u/mkenney/phpunit/trigger/6cfc768e-a07b-48c1-890c-24bbd071261c/
+elif [ "master" = "$TRAVIS_BRANCH" ]; then
+    echo "Triggering build for tag 'php5'"
+    curl -H "Content-Type: application/json" --data '{"docker_tag": "php5"}' -X POST https://registry.hub.docker.com/u/mkenney/phpunit/trigger/6cfc768e-a07b-48c1-890c-24bbd071261c/
 fi
-
-echo "TRAVIS_BRANCH: $TRAVIS_BRANCH"
-echo "DOCKER_TAG: $DOCKER_TAG"
-echo curl -H "Content-Type: application/json" --data '{"source_type": "Branch", "source_name": "'$DOCKER_TAG'"}' -X POST https://registry.hub.docker.com/u/mkenney/phpunit/trigger/6cfc768e-a07b-48c1-890c-24bbd071261c/
-curl -H "Content-Type: application/json" --data '{"source_type": "Branch", "source_name": "$DOCKER_TAG"}' -X POST https://registry.hub.docker.com/u/mkenney/phpunit/trigger/6cfc768e-a07b-48c1-890c-24bbd071261c/
