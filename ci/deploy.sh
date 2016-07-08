5ac67383e06f2ffbd9513739270b6cffda7fe90e
Michael Kenney <michael.kenney@returnpath.net>
2016-07-08 15:59:10 -0600

    Fixing the PHP5 tag name

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -4,7 +4,7 @@ if [ "master" = "$TRAVIS_BRANCH" ]; then
     echo "Triggering builds for tags 'latest' and 'php7'"
     curl -H "Content-Type: application/json" --data '{"docker_tag": "latest"}' -X POST https://registry.hub.docker.com/u/mkenney/phpunit/trigger/6cfc768e-a07b-48c1-890c-24bbd071261c/
     curl -H "Content-Type: application/json" --data '{"docker_tag": "php7"}' -X POST https://registry.hub.docker.com/u/mkenney/phpunit/trigger/6cfc768e-a07b-48c1-890c-24bbd071261c/
-elif [ "master" = "$TRAVIS_BRANCH" ]; then
+elif [ "php5" = "$TRAVIS_BRANCH" ]; then
     echo "Triggering build for tag 'php5'"
     curl -H "Content-Type: application/json" --data '{"docker_tag": "php5"}' -X POST https://registry.hub.docker.com/u/mkenney/phpunit/trigger/6cfc768e-a07b-48c1-890c-24bbd071261c/
 fi
