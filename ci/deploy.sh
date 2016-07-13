8a0ba44119fa15f8017f1ca58ebb2a3004ca565f
Michael Kenney <michael.kenney@returnpath.net>
2016-07-12 20:28:14 -0600

    Branch name fix

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env sh
 
 echo "Triggering builds for branch '$TRAVIS_BRANCH'"
-echo curl -H "Content-Type: application/json" --data "{\"source_type\": \"Branch\", \"source_name\": \"$TRAVIS_BRANCH\"}" -X POST https://registry.hub.docker.com/u/mkenney/phpunit/trigger/DOCKER_TOKEN/
-curl -H "Content-Type: application/json" --data '{"source_type": "Branch", "source_name": "$TRAVIS_BRANCH"}' -X POST https://registry.hub.docker.com/u/mkenney/phpunit/trigger/$DOCKER_TOKEN/
+curl -H "Content-Type: application/json" --data "{\"source_type\": \"Branch\", \"source_name\": \"$TRAVIS_BRANCH\"}" -X POST https://registry.hub.docker.com/u/mkenney/phpunit/trigger/$DOCKER_TOKEN/
