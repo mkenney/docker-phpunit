16a0fdc076e202aa0716d5a342c0d0a2e921ce0e
Michael Kenney <michael.kenney@returnpath.net>
2016-07-12 20:17:24 -0600

    Token test

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
 
 echo "Triggering builds for branch '$TRAVIS_BRANCH'"
+echo curl -H "Content-Type: application/json" --data '{"source_type": "Branch", "source_name": "$TRAVIS_BRANCH"}' -X POST https://registry.hub.docker.com/u/mkenney/phpunit/trigger/$DOCKER_TOKEN/
 curl -H "Content-Type: application/json" --data '{"source_type": "Branch", "source_name": "$TRAVIS_BRANCH"}' -X POST https://registry.hub.docker.com/u/mkenney/phpunit/trigger/$DOCKER_TOKEN/
